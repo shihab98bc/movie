90488515733bd4669704c78fc8f40ae5a19bec07
MD Shihab Uddin <120728054+shihab98bc@users.noreply.github.com>
2025-03-19 12:13:49 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import MovieDetail from './components/MovieDetail';
 import TvDetail from './components/TvDetail';
 import SearchResult from './components/SearchResult';
 import Footer from './components/ui/Footer';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
@@ -26,8 +25,6 @@ function App() {
           <Route path='/tv/:id' element={<TvDetail />} />
 
           <Route path="/search/:query" element={<SearchResult />} />
-
-          <Route path="*" element={<SpeedInsights />} />
 
         </Routes>
       </Router>
