bad83df2057e98ba38400d83f292627ad4ac901a
MD Shihab Uddin <120728054+shihab98bc@users.noreply.github.com>
2025-03-22 05:38:23 +0600

    Update Navbar.jsx

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+raimport React from 'react';
 import ThemeToggle from './ToggleTheme';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -23,7 +23,7 @@ const Navbar = () => {
     <div className="z-[20] px-5 sm:px-8 mx-auto border-b top-0 left-0 bg-gray-200/60 dark:bg-[#6d6d6d88] w-full dark:text-white text-gray-900">
       <nav className="px-3 sm:px-8 py-3">
         <div className="container mx-auto flex sm:flex-row flex-col flex-wrap items-center justify-between">
-          <Link to='/'><span className="mt-1 sm:mt-0 inline-block self-center text-lg select-none font-semibold whitespace-nowrap">Movies & Tv Shows</span>
+          <Link to='/'><span className="mt-1 sm:mt-0 inline-block self-center text-lg select-none font-semibold whitespace-nowrap">EloraFilm</span>
           </Link>
           <div className="relative ">
             <input
